d7d4622dbe903e2e34febc739b22a6e4ba89c2d8
rahmatillo <rahmatillo>
2025-08-19 14:13:57 +0500

    10-dars

---
+++ modul.mjs
@@ -0,0 +1,22 @@
+import {
+  ShowAgeD,
+  printName,
+  AddNumberD,
+  greet,
+  fullName,
+  rectangPerimetr,
+  triangleSides,
+  squareNumber,
+  animalInfo,
+  sayHello,
+} from "./function.js";
+ShowAgeD(20);
+AddNumberD(5, 7);
+printName("Ali", "Vali", "Aliya", "ALibobo", "Salom");
+greet("Ali");
+fullName("Ali", "Valiyev");
+rectangPerimetr(10, 5);
+triangleSides(3, 4, 5);
+squareNumber(6);
+animalInfo("it", 3);
+sayHello();
